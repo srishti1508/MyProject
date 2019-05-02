daa748ffe19ba0ad224c17fd3c7c796590a05550
Pankaj <rajak.pankaj2@gmail.com>
2019-05-02 16:48:22 +0530

    initial

--- app/src/main/java/tgs/com/myproject/MainActivity.java
+++ app/src/main/java/tgs/com/myproject/MainActivity.java
@@ -2,12 +2,21 @@ package tgs.com.myproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text=findViewById(R.id.ttext);
+        text.setText("Hi....");
+
+
+
+
+
+
     }
 }
